20be6015f10453ccdaf7cc34832c2e4dd6a4e2a7
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-03 13:41:35 +0530

    [sasic] Refactor . Calculating For a Month

--- GamblingSimulatorWorskShop/Program.cs
+++ GamblingSimulatorWorskShop/Program.cs
@@ -10,7 +10,7 @@ namespace GamblingSimulatorWorskShop
         /// </summary>
         public const int STARTING_DAILY_STAKE = 100;
         public const int BET = 1;
-        public const int DAYS = 20;
+        public const int DAYS = 30;
 
         /// variales
         public static int totalWin = 0;
@@ -24,21 +24,20 @@ namespace GamblingSimulatorWorskShop
         {
             Console.WriteLine("Welcome to Gambling simulator");
             int dayAmount = STARTING_DAILY_STAKE;
-            int checkWin = RandomCheck();
+            int dailyWin = 0;
+            int dailyLoss = 0;
 
             /// UC4
             for (int i=0; i < DAYS; i++)
             {
-                int dailyWin = 0;
-                int dailyLoss = 0;
-                while (checkResign(STARTING_DAILY_STAKE, dayAmount) != 1)
+                while (CheckResign(STARTING_DAILY_STAKE, dayAmount) != 1)
                 {
                     /// It Checks if returned values is 1 then gambler win 
                     /// It not gamber not win
-                    if (checkWin == 1)
+                    if (RandomCheck() == 1)
                     {
                         dayAmount +=BET;
-                        dailyWin +=BET;
+                        dailyWin += BET;
                     }
                     else
                     {
@@ -46,15 +45,12 @@ namespace GamblingSimulatorWorskShop
                         dailyLoss += BET;
                     }
                 }
-                totalWin += dailyWin;
-                totalLoss += dailyLoss;
             }
-
-            Console.WriteLine("Starting Daily stake :" + STARTING_DAILY_STAKE);
-            Console.WriteLine("Bet :" + BET);
+            totalWin += dailyWin;
+            totalLoss += dailyLoss;
+            Console.WriteLine("On day " + DAYS + "\tgambler won:\t" + dailyWin + "\tand lost:\t" + dailyLoss);
             Console.WriteLine("Total win :" + totalWin);
             Console.WriteLine("Total loss :" + totalLoss);
-
         }
 
         /// <summary>
@@ -77,10 +73,10 @@ namespace GamblingSimulatorWorskShop
         /// <param name="STARTING_DAILY_STAKE">The starting daily stake.</param>
         /// <param name="dayAmount">The day amount.</param>
         /// <returns></returns>
-        private static int checkResign(int STARTING_DAILY_STAKE, int dayAmount)
+        private static int CheckResign(int startingStake, int dayAmount)
         {
-            double winResigmAmt = 1.5 * STARTING_DAILY_STAKE;
-            double lossResignAmt = 0.5 * STARTING_DAILY_STAKE;
+            double winResigmAmt = 1.5 * startingStake;
+            double lossResignAmt = 0.5 * startingStake;
             if (dayAmount >= winResigmAmt || dayAmount <= lossResignAmt)
                 return 1;
             else
