7851a16b70a2116cc7e48240f064bd1bc90556a7
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-03 10:04:35 +0530

    Add project files.

---
+++ GamblingSimulatorWorskShop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GamblingSimulatorWorskShop
+{
+    public class Program
+    {
+        /// <summary>
+        /// constants
+        /// </summary>
+        public const int STARTING_DAILY_STAKE = 100;
+        public const int BET = 1;
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling simulator");
+            Console.WriteLine("Starting Daily stake :"+ STARTING_DAILY_STAKE);
+            Console.WriteLine("Bet :" + BET);
+        }
+    }
+}
